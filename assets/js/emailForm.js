08c66a667e0b76302786a25c641714f0086933b4
UiWill <williamemanoel777@gmail.com>
2024-10-30 14:12:50 -0300

    nova atualização

---
+++ assets/js/emailForm.js
@@ -0,0 +1,32 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_bs6dqlw";
+    const templateID = "template_al6i2zk";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("phone").value = "";
+          document.getElementById("subject").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Menssagem enviada com sucesso!!")
+  
+      })
+      .catch(err => {
+        console.error("Erro ao enviar o email:", err);
+        alert("Ocorreu um erro ao enviar a mensagem. Tente novamente.");
+    });
+    
+  
+  }
+  
+  
