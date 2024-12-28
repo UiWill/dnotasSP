98449295b8504bae8996b5b16eaeb50ac1792f7f
UiWill <williamemanoel777@gmail.com>
2024-12-28 16:25:29 -0300

    Automatico

--- assets/js/actionButton.js
+++ assets/js/actionButton.js
@@ -37,6 +37,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Ação ao clicar no botão do WhatsApp
     whatsappButton.addEventListener('click', function () {
-        window.location.href = 'https://wa.me/553791979016';
+        window.location.href = 'https://wa.me/5537991555222';
     });
 });
